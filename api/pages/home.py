9b648c955a44ad09cede28e86bc6579b746ce166
Rajvir3104 <rajvirz2004@gmail.com>
2026-05-13 12:43:35 -0600

    parquet allowed

--- api/pages/home.py
+++ api/pages/home.py
@@ -13,7 +13,7 @@ layout = html.Div([
             "Drag and Drop or ",
             html.A("Select CSV File")
         ]),
-        accept=".csv,.txt,.parquet",
+        accept=".parquet",
         multiple=False,
         style={
             "width": "100%",
